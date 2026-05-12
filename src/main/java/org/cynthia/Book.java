2d5adbb0db9d7087f56bbee85a93081cf2a21356
Cynthia <ouocwouo@gmail.com>
2026-05-12 19:10:46 -0400

    add field,lombok method constructor in Book class

--- src/main/java/org/cynthia/Book.java
+++ src/main/java/org/cynthia/Book.java
@@ -1,4 +1,21 @@
 package org.cynthia;
 
-public class Book {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Setter
+@Getter
+@ToString
+public class Book extends Item{
+    private String ISBN;
+    private String author;
+    private String genre;
+
+    public Book(String id, String title, Status status, String ISBN, String author, String genre) {
+        super(id, title, status);
+        this.ISBN = ISBN;
+        this.author = author;
+        this.genre = genre;
+    }
 }
