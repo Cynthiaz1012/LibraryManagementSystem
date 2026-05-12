ed888571b0dd06cd71e5847c12a6c4e29034f6a5
Cynthia <ouocwouo@gmail.com>
2026-05-12 18:19:18 -0400

    add lombok method and isAvailable method in Item class

--- src/main/java/org/cynthia/Item.java
+++ src/main/java/org/cynthia/Item.java
@@ -1,9 +1,24 @@
 package org.cynthia;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
 public abstract class Item {
     private String id;
     private String title;
     private Status status;
+
+    /**
+     * Checks whether item is available.
+     * @return true if available
+     */
+    public boolean isAvailable() {
+        return status == Status.AVAILABLE;
+    }
 
     public enum Status {
         AVAILABLE,
