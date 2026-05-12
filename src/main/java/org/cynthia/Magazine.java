262c1a4a1c6cfab5b5b4699e750a9f7f9edd8875
Cynthia <ouocwouo@gmail.com>
2026-05-12 19:29:11 -0400

    add field,lombok method constructor in Magazine class

--- src/main/java/org/cynthia/Magazine.java
+++ src/main/java/org/cynthia/Magazine.java
@@ -1,4 +1,19 @@
 package org.cynthia;
 
-public class Magazine {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class Magazine extends Item {
+    private int issueNumber;
+    private String publisher;
+
+    public Magazine(String id, String title, Status status, int issueNumber, String publisher) {
+        super(id, title, status);
+        this.issueNumber = issueNumber;
+        this.publisher = publisher;
+    }
 }
