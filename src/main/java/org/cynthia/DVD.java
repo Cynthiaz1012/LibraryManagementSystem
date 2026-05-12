dc1ebfd99245b42fca8fe0b9e8d0130f244020e8
Cynthia <ouocwouo@gmail.com>
2026-05-12 19:19:35 -0400

    add field,lombok method constructor in DVD class

--- src/main/java/org/cynthia/DVD.java
+++ src/main/java/org/cynthia/DVD.java
@@ -1,4 +1,19 @@
 package org.cynthia;
 
-public class DVD {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class DVD extends Item{
+    private String director;
+    private int duration;
+
+    public DVD(String id, String title, Status status, String director, int duration) {
+        super(id, title, status);
+        this.director = director;
+        this.duration = duration;
+    }
 }
