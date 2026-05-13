1291d2d6f5d0ed9a75d36c1d4ce1f862b67af4e5
Cynthia <ouocwouo@gmail.com>
2026-05-12 21:44:21 -0400

    add lombok method and constructor in Teacher class

--- src/main/java/org/cynthia/Teacher.java
+++ src/main/java/org/cynthia/Teacher.java
@@ -1,4 +1,16 @@
 package org.cynthia;
 
-public class Teacher {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public class Teacher extends User {
+    public Teacher(String id, String name, List<Item> borrowedItems) {
+        super(id, name, borrowedItems);
+    }
 }
