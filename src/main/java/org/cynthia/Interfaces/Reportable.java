3c5c34c6b7d1dfa3d5c858de822e987da1983dcb
Cynthia <ouocwouo@gmail.com>
2026-05-13 13:11:36 -0400

    modify the method in interface

--- src/main/java/org/cynthia/Interfaces/Reportable.java
+++ src/main/java/org/cynthia/Interfaces/Reportable.java
@@ -1,10 +1,5 @@
 package org.cynthia.Interfaces;
 
 public interface Reportable {
-    /**
-     * Generates a report of all library items based on their current status.
-     * @param status The status of items to include in the report
-     * @return A string representation of the report.
-     */
-    String generateReport(String status);
+        void generateReport();
 }
