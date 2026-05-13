e86b93f6f8087efd0dcff1ee16b57e9521333b44
Cynthia <ouocwouo@gmail.com>
2026-05-13 16:22:58 -0400

    change the method in interface

--- src/main/java/org/cynthia/Interfaces/Reportable.java
+++ src/main/java/org/cynthia/Interfaces/Reportable.java
@@ -1,5 +1,8 @@
 package org.cynthia.Interfaces;
 
+import org.cynthia.domain.Item;
+import java.util.List;
+
 public interface Reportable {
-        void generateReport();
+    void generateReport(List<Item> items);
 }
