343d2a1673890f241b4b7de502eb65f3d3f459a2
Cynthia <ouocwouo@gmail.com>
2026-05-13 13:49:31 -0400

    Initialize Library class

--- src/main/java/org/cynthia/domain/Library.java
+++ src/main/java/org/cynthia/domain/Library.java
@@ -1,4 +1,24 @@
 package org.cynthia.domain;
 
-public class Library {
+import org.cynthia.Interfaces.Reportable;
+
+import java.util.*;
+
+public class Library implements Reportable {
+    private List<Item> items;
+    private List<User> users;
+
+    private Queue<User> waitingQueue;
+    private Stack<String> logs;
+    private Set<String> borrowedItemIds;
+    private Map<String, Item> itemMap;
+
+    public Library() {
+        items = new ArrayList<>();
+        users = new ArrayList<>();
+        waitingQueue = new LinkedList<>();
+        logs = new Stack<>();
+        borrowedItemIds = new HashSet<>();
+        itemMap = new HashMap<>();
+    }
 }
