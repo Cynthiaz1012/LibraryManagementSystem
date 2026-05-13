754b3db65cc069363666273983da55f85dda510e
Cynthia <ouocwouo@gmail.com>
2026-05-12 21:17:15 -0400

    add constructor and BookOnly method in Student class

--- src/main/java/org/cynthia/Student.java
+++ src/main/java/org/cynthia/Student.java
@@ -1,4 +1,19 @@
 package org.cynthia;
 
-public class Student {
+
+import java.util.List;
+
+public class Student extends User{
+    public Student(String id, String name, List<Item> borrowedItems) {
+        super(id, name, borrowedItems);
+    }
+
+    /**
+     * Students can only borrow books.
+     * @param item The item to check.
+     * @return True if item is a Book, false otherwise.
+     */
+    public boolean canBorrowItem(Item item) {
+        return item instanceof Book;
+    }
 }
