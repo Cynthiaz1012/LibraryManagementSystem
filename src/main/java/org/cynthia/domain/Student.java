fccfff48ffb7ac79278b5162068370cd26e0732f
Cynthia <ouocwouo@gmail.com>
2026-05-13 16:13:35 -0400

    modify Student class's borrow method and use constants

--- src/main/java/org/cynthia/domain/Student.java
+++ src/main/java/org/cynthia/domain/Student.java
@@ -4,29 +4,21 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
-import java.util.List;
+import static org.cynthia.util.Constants.MAX_BOOKS_STUDENT;
 
 @Getter
 @Setter
 @ToString
 public class Student extends User {
-    public Student(String id, String name, List<Item> borrowedItems) {
-        super(id, name, borrowedItems);
+    public Student(String id, String name) {
+        super(id, name);
     }
 
     @Override
-    public int getBorrowingLimit() {
-        return 5;
-    }
-
-    @Override
-    public void borrowItem(Item item) {
+    public boolean canBorrow(Item item) {
         if (!(item instanceof Book)) {
-            throw new IllegalArgumentException("Students can only borrow books.");
+            return false;
         }
-        if (borrowedItems.size() >= getBorrowingLimit()) {
-            throw new IllegalArgumentException("Student borrow limit reached.");
-        }
-        borrowedItems.add(item);
+        return borrowedItems.size() < MAX_BOOKS_STUDENT;
     }
 }
