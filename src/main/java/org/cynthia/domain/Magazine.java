01147febae4ffaa4cb125a1b702a696eed89fa76
Cynthia <ouocwouo@gmail.com>
2026-05-13 15:27:48 -0400

    modify Magazine class

--- src/main/java/org/cynthia/domain/Magazine.java
+++ src/main/java/org/cynthia/domain/Magazine.java
@@ -11,9 +11,14 @@ public class Magazine extends Item {
     private int issueNumber;
     private String publisher;
 
-    public Magazine(String id, String title, Status status, int issueNumber, String publisher) {
-        super(id, title, status);
+    public Magazine(String id, String title, int issueNumber, String publisher) {
+        super(id, title);
         this.issueNumber = issueNumber;
         this.publisher = publisher;
+    }
+
+    @Override
+    public String getDetails() {
+        return "Magazine: " + title + " published by " + publisher;
     }
 }
