4a81a646d71447203c1f280f9a8c8ae04b07ada5
Cynthia <ouocwouo@gmail.com>
2026-05-12 21:03:37 -0400

    add field,lombok method constructor in User class

--- src/main/java/org/cynthia/User.java
+++ src/main/java/org/cynthia/User.java
@@ -1,4 +1,22 @@
 package org.cynthia;
 
-public class User {
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.List;
+
+@Getter
+@Setter
+@ToString
+public abstract class User {
+    protected String id;
+    protected String name;
+    protected List<Item> borrowedItems;
+
+    public User(String id, String name, List<Item> borrowedItems) {
+        this.id = id;
+        this.name = name;
+        this.borrowedItems = borrowedItems;
+    }
 }
