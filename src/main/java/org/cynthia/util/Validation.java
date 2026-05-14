4db47333befb38086df211e4662fdb5d6a0a6c08
Cynthia <ouocwouo@gmail.com>
2026-05-13 23:17:51 -0400

    add documentation for the method isValidISBN in Validation class

--- src/main/java/org/cynthia/util/Validation.java
+++ src/main/java/org/cynthia/util/Validation.java
@@ -1,6 +1,11 @@
 package org.cynthia.util;
 
 public class Validation {
+    /**
+     * Checks if an ISBN is valid.
+     * @param isbn the ISBN number
+     * @return true if valid, otherwise false
+     */
     public static boolean isValidISBN(String isbn) {
         return isbn.matches("\\d{13}");
     }
