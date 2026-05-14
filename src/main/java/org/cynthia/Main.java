8b01f649bf62860280f502d6ecbac8beaead3dc3
Cynthia <ouocwouo@gmail.com>
2026-05-13 21:20:13 -0400

    main class

--- src/main/java/org/cynthia/Main.java
+++ src/main/java/org/cynthia/Main.java
@@ -1,7 +1,17 @@
 package org.cynthia;
 
+import org.cynthia.domain.Book;
+import org.cynthia.domain.Library;
+import org.cynthia.domain.Student;
+
 public class Main {
     static void main() {
-
+        Library library = new Library();
+        Book book1 = new Book(1, "Java Basics", "1234567890123", "Tom", "Programming");
+        Student student = new Student(100, "Alice");
+        library.addItem(book1);
+        library.addUser(student);
+        library.borrowItem(100, 1);
+        System.out.println(student.getBorrowedItems());
     }
 }
